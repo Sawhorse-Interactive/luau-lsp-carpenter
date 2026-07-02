eceb82f70998094383a0d234dfc3199e20dc7cf0
codesenseAye <44715949+codesenseAye@users.noreply.github.com>
2026-07-02 01:15:54 +0000

    Update types dump

--- scripts/globalTypes.d.lua
+++ scripts/globalTypes.d.lua
@@ -8750,13 +8750,13 @@ type VirtualInputPointerAction = {
 }
 
 
+type ReflectedClasses = any
 type ReflectedClassOrNil = any
-type ReflectedProperties = any
-type VideoSampleArray = any
 type InfoTypeArray = any
 type RankedItemArray = any
+type ReflectedProperties = any
 type ProductIdentifierArray = any
-type ReflectedClasses = any
+type VideoSampleArray = any
 
 declare class Object
 	@deprecated
